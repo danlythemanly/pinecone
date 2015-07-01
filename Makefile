a26623c3b13bdddc23b377f381672a2071437c6d
Dan Williams <danlythemanly@gmail.com>
2015-06-30 20:25:45 -0400

    before going for sin wave

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ OBJCOPY=/usr/share/arduino/hardware/tools/avr/bin/avr-objcopy
 AVRDUDE=/usr/share/arduino/hardware/tools/avrdude
 
 CFLAGS=-g -DF_CPU=8000000L -Wall -Os -Werror -Wextra -mmcu=attiny85
+# for 1 MHz
+#CFLAGS=-g -DF_CPU=1000000L -Wall -Os -Werror -Wextra -mmcu=attiny85
 OFLAGS=-j .text -j .data -O ihex
 
 all: hello.hex
@@ -19,6 +21,10 @@ hello.hex: hello.elf
 
 fuses: 
 	$(AVRDUDE) -C/home/djwillia/dev/arduino-1.6.4/hardware/tools/avr/etc/avrdude.conf -v -v -v -v -pattiny85 -cstk500v1 -P/dev/ttyACM0 -b19200 -e -Uefuse:w:0xff:m -Uhfuse:w:0xdf:m -Ulfuse:w:0xe2:m
+
+# for 1 MHz
+#    $(AVRDUDE) -C/home/djwillia/dev/arduino-1.6.4/hardware/tools/avr/etc/avrdude.conf -v -v -v -v -pattiny85 -cstk500v1 -P/dev/ttyACM0 -b19200 -e -Uefuse:w:0xff:m -Uhfuse:w:0xdf:m -Ulfuse:w:0x62:m
+
 
 install: hello.hex
 	stty -F /dev/ttyACM0 cs8 19200 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
--- hello.c
+++ hello.c
@@ -2,16 +2,17 @@
 #include <util/delay.h>
 
 
-#define LED PB0
-#define BUTTON PB1
+#define LED PB4
+#define BUTTON PB3
+#define SPEAKER PB1
 #define DEBOUNCE 100
 
 #define led_on() do {							\
-		PORTB |= (1 << PB0);					\
+		PORTB |= (1 << LED);					\
 	} while (0)
 
 #define led_off() do {							\
-		PORTB &= ~(1 << PB0);					\
+		PORTB &= ~(1 << LED);					\
 	} while (0)
 
 struct button {
@@ -60,22 +61,119 @@ int main(void) {
 	struct button b;
 
 	/* make LED an output */
-	DDRB |= 1 << LED;
+	DDRB |= (1 << LED) | (1 << SPEAKER);
 	/* enable pull-up resistor for BUTTON */
 	PORTB |= 1 << BUTTON;
-	
+
+#if 0
+	/* Fast PWM, top is 0xFF, non-inverting */
+	TCCR0A |= (1 << WGM01) | (1 << WGM00) | (1 << COM0A1);// | (1 << COM0A0);
+	/* 64 pre-scale ~= 488 Hz*/
+	TCCR0B |= (1 << CS00) | (1 << CS02);// | (1 << CS00);
+
+	OCR0A = 128;
+#endif	
+
+#define OCR0A_VAL 0x81
+
+#if 1
+	/* fast PWM non-inverting, prescaler /256 */
+	TCCR0A = (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
+	TCCR0B = (1 << WGM02) | (1<<CS02) | (0<<CS01) | (0<<CS00);
+	TCNT0=0x00;
+	OCR0A=0x81;
+	//OCR0B=0x41;
+	OCR0B=0x00;
+#endif
+
 	led_off();
 	button_init(&b);
 
+	int count = 0;
+	//int count2 = 1000;
+#if 1
+#define NUM_NOTES 16
+	int note[NUM_NOTES];
+	int n = 0;
+#endif
+	//	note[0] = 0xff;
+	//	note[1] = 0xb0;
+	//note[0] = 0x80;
+	//note[1] = 0x70;
+
+#if 0
+	note[0] = 239;
+	note[1] = 225;  
+	note[2] = 213; 
+	note[3] = 201;
+	note[4] = 190;
+	note[5] = 179;
+	note[6] = 169;
+	note[7] = 159;  
+	note[8] = 150; 
+	note[9] = 142;
+	note[10] = 134;
+	note[11] = 127;
+#endif
+
+#if 0
+	note[0] = 0x60;
+	note[1] = 0x55;  
+	note[2] = 0x4c; 
+	note[3] = 0x48; /* 4th */
+	note[4] = 0x40; /* 5th */
+	note[5] = 0x00;
+#endif
+
+#if 1
+	note[0] = 0x60;
+	note[1] = 0xc0;
+	note[2] = 0x60;
+	note[3] = 0xc0;
+	note[4] = 0x40;
+	note[5] = 0x0;
+	note[6] = 0x40;
+	note[7] = 0x0;
+	note[8] = 0x39;
+	note[9] = 0x0;
+	note[10] = 0x39;
+	note[11] = 0x0;
+	note[12] = 0x40;
+	note[13] = 0x40;
+	note[14] = 0x40;
+	note[15] = 0x0;
+#endif
 	while(1) {
 		check_button(&b);
+#if 0
+		if (count++ % 1000 == 0){
+		    OCR0B = (OCR0B + 1) % (OCR0A_VAL - 1) + 1;
+		}
+#endif
+
+#if 1
+		//if ((count++ == 0) && (count2++ % 2 == 0)){
+		if ((count++ == 0)){
+			OCR0A = note[n];
+			n = (n + 1) % NUM_NOTES;
+		}
+#endif
+
+#if 0
+		if (count++ > 30000){
+			TCCR0B ^= (1 << CS02);
+ 			TCNT0 =0;
+			count = 0;
+		}
+#endif
+
+		/* I don't think we need to use a second timer to advance
+		   through the waveform with the appropriate divider for a
+		   particular note... */
 	}
 
 
 #if 0
-		PORTB &= ~(1 << PB0); 
-		_delay_ms(1000);
-		PORTB |= (1 << PB0);
 		_delay_ms(1000);
 #endif
 
